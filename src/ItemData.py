0f538f793b6dc8e072eaece1656c8f5a7223bdc5
sugar6400 <sugar6400@gmail.com>
2018-11-07 12:29:53 +0900

    アイテムと価格データのクラス設定

---
+++ src/ItemData.py
@@ -0,0 +1,34 @@
+
+class ItemData(object):
+    id = -1
+    name = "未定義アイテム"
+    category = "未定義カテゴリ"
+    scale = "個(未定義)"
+    sort = -1
+
+    def __init__(self, id_in, name, category, scale, sort):
+        self.id = id_in
+        self.name = name
+        self.category = category
+        self.scale = scale
+        self.sort = sort
+
+    @staticmethod
+    def from_dict(source):
+        item_data = ItemData(source['id'], source[u'name'], source[u'category'], source[u'scale'], source[u'sort'])
+        return item_data
+
+    def to_dict(self):
+        dest = {
+            u'id': self.id,
+            u'name': self.name,
+            u'category': self.category,
+            u'scale': self.scale,
+            u'sort': self.sort
+        }
+        return dest
+
+    def __repr__(self):
+        return u'ItemData(id:{} name:{} category:{} scale:{} sort:{})'.format(
+            self.id, self.name, self.category, self.scale, self.sort)
+
