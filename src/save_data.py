9198252a815a702d7cbe3ca9aa37cfde81e34ac3
sugar6400 <sugar6400@gmail.com>
2018-11-28 22:36:21 +0900

    save_data.pyのデータ構造について修正(converterはまだ)

--- src/save_data.py
+++ src/save_data.py
@@ -1,7 +1,11 @@
 import pickle
 import datetime
 import os
+from datetime import datetime
+from typing import List, Dict, Any
+
 from src import sale_data
+from src.sale_data import SaleList
 
 save_dir = 'save'
 graph_dir = 'save/graph'
@@ -21,45 +25,46 @@ class GraphData(object):
 
 class SaveItem(object):
 
+    today_graph: List[GraphData]
+    week_sale_lists: List[List[SaleList]]
+
     def __init__(self, sale):
         # week_sales[7*120]
-        self.week_sale_lists = [sale_data.SaleList(sale)]  # type: list
-        # hour_sales[<5]
-        self.hour_sale_lists = [sale_data.SaleList(sale)]  # type: list
+        self.week_sale_lists = [[sale_data.SaleList(sale)]]
         # today_graph[<24]
-        self.today_graph = []  # type: list[GraphData]
+        self.today_graph = []
 
     # データの追加
     def add(self, sale):
-        self.week_sale_lists[0].add(sale)
-        self.hour_sale_lists[0].add(sale)
+        self.week_sale_lists[0][0].add(sale)
 
     # 日付が変わったらグラフデータをファイルに出力
-    def change_day(self):
-        day_str = str(datetime.date.today() + datetime.timedelta(days=-1))
+    def change_day(self, old):
+        day_str = str(old)
         with open(os.path.join(graph_dir, graph_file_name + day_str + '.pkl'), mode='wb') as f:
             pickle.dump(self.today_graph, f)
         self.today_graph = []
+        self.week_sale_lists.insert(0, [])
+        self.week_sale_lists = self.week_sale_lists[:7]
 
     # 時間が変わったら一時間の平均を今日のグラフデータに追加して一時間のデータを消す
-    def change_hour(self, old, new):
-        sum_price = 0
-        sum_num = 0
-        if self.hour_sale_lists:
-            for sale_list in self.hour_sale_lists:
-                sum_price = sum_price + sale_list.sum_price()
-                sum_num = sum_num + sale_list.sum_num()
-            self.today_graph.append(GraphData(sum_price/sum_num, old))
-            self.hour_sale_lists.insert(0, sale_data.SaleList())
+    def change_hour(self, old):
+        sale_list = self.week_sale_lists[0][0]
+        if sale_list:
+            self.today_graph.append(GraphData(sale_list.sum_price() / sale_list.sum_num(), old))
 
     def __repr__(self):
-        return 'SaveItem: \n \tweek_sales:{} \n \ttoday_graph:{}'.format(self.week_sale_lists, self.today_graph)
+        return 'SaveItem \n \tweek_sales:{} \n \ttoday_graph:{}'.format(self.week_sale_lists, self.today_graph)
 
 
 class SaveData(object):
 
-    def __init__(self, day=datetime.datetime.today().day, hour=datetime.datetime.today().hour):
-        self.save_items = {}  # type: dict[str, SaveItem]
+    hour: int
+    day: int
+    save_items: Dict[str, SaveItem]
+
+    def __init__(self, day=datetime.today().day, hour=datetime.today().hour):
+        self.save_items = {}
         self.day = day
         self.hour = hour
         self.mod_time = datetime.datetime.today()
@@ -67,13 +72,13 @@ class SaveData(object):
                 os.path.exists(os.path.join(save_dir, 'time_data.pkl')):
             self.load()
 
-    def on_change_day(self):
+    def on_change_day(self, old):
         for item in self.save_items.values():
-            item.change_day()
+            item.change_day(old)
 
-    def on_change_hour(self, old, new):
+    def on_change_hour(self, old):
         for item in self.save_items.values():
-            item.change_hour()
+            item.change_hour(old)
 
     def add_sale(self, item_id_str, sale):
         # すでに同じIDのアイテムがあるなら追加，ないなら新規作成
@@ -91,12 +96,12 @@ class SaveData(object):
 
         # 日付が変わった
         if self.day != datetime.date.today().day:
+            self.on_change_day(self.day)
             self.day = datetime.date.today().day
-            self.on_change_day()
 
         # 時間が変わった
         if self.hour != datetime.datetime.today().hour:
-            self.on_change_hour(self.hour, datetime.datetime.today().hour)
+            self.on_change_hour(self.hour)
             self.hour = datetime.datetime.today().hour
 
         n_loaded = 0
@@ -113,7 +118,6 @@ class SaveData(object):
         with open(os.path.join(save_dir, 'time_data.pkl'), mode='wb') as f:
             pickle.dump(self.day, f)
             pickle.dump(self.hour, f)
-            pickle.dump(datetime.datetime.today(), f)
 
     def save(self):
         with open(os.path.join(save_dir, 'save_data.pkl'), mode='wb') as f:
@@ -126,4 +130,3 @@ class SaveData(object):
         with open(os.path.join(save_dir, 'time_data.pkl'), mode='rb') as f:
             self.day = pickle.load(f)
             self.hour = pickle.load(f)
-            self.mod_time = pickle.load(f)
